b5a84be748e4b499ccc8ba2e723c21926e085da2
JoeyDallaire <32402889+JoeyDallaire@users.noreply.github.com>
2024-01-12 10:55:11 -0500

    Test1
    Changed a comment  in  a script file to test github

--- ItsSpreading/Assets/Scripts/Entity.cs
+++ ItsSpreading/Assets/Scripts/Entity.cs
@@ -7,7 +7,7 @@ public class Entity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // This is a test for github!
     }
 
     // Update is called once per frame
