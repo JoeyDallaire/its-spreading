6992a79582a4130ca119d548c41780fe796f6a10
JoeyDallaire <32402889+JoeyDallaire@users.noreply.github.com>
2024-01-14 08:33:34 -0500

    TextMeshProImport + animation test

---
+++ ItsSpreading/Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+    private const int MAX_CHAR_LENGHT = 100;
+    
+    private string text;
+    
+    
+    public DialogueBox(string text)
+    {
+        this.text = text;
+    }
+}
---
+++ ItsSpreading/Assets/Test_Stuff/test_player_controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_player_controller : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private const float SPEED = 1f;
+    private float currentSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentSpeed = SPEED;
+        }
+        else currentSpeed = 0;
+        
+        
+        _animator.SetFloat("speed_anim", currentSpeed);
+
+    }
+}
